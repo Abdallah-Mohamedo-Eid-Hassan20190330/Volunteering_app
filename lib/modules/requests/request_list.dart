e8fc6fc5ed2c61049e997b84ceba00ee3f9df334
Islam Ahmed <62517820+Hakugami@users.noreply.github.com>
2023-07-02 01:01:32 +0300

    added functionality to the accept button

--- lib/modules/requests/request_list.dart
+++ lib/modules/requests/request_list.dart
@@ -1,8 +1,8 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:volunteer_app/firebase/firebase_helper.dart';
 import 'package:volunteer_app/models/Request.dart';
 
 class RequestList extends StatefulWidget {
@@ -11,61 +11,104 @@ class RequestList extends StatefulWidget {
 }
 
 class _RequestListState extends State<RequestList> {
-  // final FirebaseFirestore firestore = FirebaseFirestore.instance;
   List<Request> users = [];
 
-  Future getData() async {
-    // users.clear();
+  Future<void> getData() async {
     FirebaseFirestore db = FirebaseFirestore.instance;
-    db.collection("Requests").get().then((queryList) {
-      for (var doc in queryList.docs) {
+    QuerySnapshot querySnapshot = await db.collection("Requests").get();
+    List<Request> requestList = querySnapshot.docs
+        .map((doc) => Request.fromJson(doc.data() as Map<String, dynamic>))
+        .toList();
+    setState(() {
+      users = requestList;
+    });
+  }
+
+  Future<void> acceptRequest(String nationalId, int index) async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    QuerySnapshot querySnapshot = await db
+        .collection("Requests")
+        .where("blindData.nationalId", isEqualTo: nationalId)
+        .get();
+    if (querySnapshot.docs.length > 0) {
+      DocumentSnapshot documentSnapshot = querySnapshot.docs[0];
+      String requestId = documentSnapshot.id;
+
+      db.collection('Requests')
+          .doc(requestId)
+          .update({'state': 'accepted'})
+          .then((_) {
         setState(() {
-          users.add(Request.fromJson(doc.data()));
-          print(users[0].blindData.nationalId);
-          print(users.length);
+          users[index].state = 'accepted';
         });
-      }
-      print(users.length);
-    }).catchError((e) {
-      Fluttertoast.showToast(
-          msg: e.toString(),
+
+        // Show visual feedback for button press
+        Timer(const Duration(milliseconds: 200), () {
+          setState(() {
+            users[index].state = 'accepted';
+          });
+        });
+
+        Fluttertoast.showToast(
+          msg: 'Request accepted successfully!',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0,
+        );
+      }).catchError((e) {
+        Fluttertoast.showToast(
+          msg: 'Failed to accept request. ${e.toString()}',
           toastLength: Toast.LENGTH_SHORT,
           gravity: ToastGravity.BOTTOM,
           timeInSecForIosWeb: 1,
           backgroundColor: Colors.red,
           textColor: Colors.white,
-          fontSize: 16.0);
-      print(e);
-    });
+          fontSize: 16.0,
+        );
+        print(e);
+      });
+    } else {
+      Fluttertoast.showToast(
+        msg: 'Request not found!',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    }
   }
 
   @override
   void initState() {
-    // TODO: implement initState
+    super.initState();
     getData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: users == null || users.isEmpty
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.separated(
-                itemBuilder: (context, index) {
-                  return buildRequestItem(users, index);
-                },
-                separatorBuilder: (context, index) => Container(
-                  width: double.infinity,
-                  color: Colors.grey,
-                ),
-                itemCount: users.length,
-              ));
+      appBar: AppBar(),
+      body: ListView.separated(
+        itemCount: users.length,
+        separatorBuilder: (context, index) => Container(
+          width: double.infinity,
+          color: Colors.grey,
+        ),
+        itemBuilder: (context, index) {
+          return buildRequestItem(users, index);
+        },
+      ),
+    );
   }
 
   Widget buildRequestItem(List<Request> users, int index) {
+    bool isAccepted = users[index].state == 'accepted';
+
     return Container(
       height: 100,
       width: double.infinity,
@@ -94,69 +137,79 @@ class _RequestListState extends State<RequestList> {
             const SizedBox(
               width: 20,
             ),
-            Container(
-              child: Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      padding: EdgeInsets.all(10),
-                      child: Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              users[index].blindData.fullName,
-                              overflow: TextOverflow.clip,
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            Text(
-                              "${users[index].blindData.phone}",
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Row(
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Container(
-                          height: 40,
-                          width: 110,
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: MaterialButton(
-                            onPressed: () {},
-                            child: Text("Accept"),
-                          ),
+                        Text(
+                          users[index].blindData.fullName,
+                          overflow: TextOverflow.clip,
                         ),
                         const SizedBox(
                           width: 20,
                         ),
-                        Container(
-                          height: 40,
-                          width: 115,
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: MaterialButton(
-                            onPressed: () {},
-                            child: Text("View on Map"),
-                          ),
+                        Text(
+                          "${users[index].blindData.phone}",
+                          overflow: TextOverflow.ellipsis,
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          if (!isAccepted) {
+                            acceptRequest(
+                                users[index].blindData.nationalId, index);
+                          }
+                        },
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          height: 40,
+                          width: 110,
+                          decoration: BoxDecoration(
+                            color: isAccepted ? Colors.grey : Colors.blue,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: Text(
+                              isAccepted ? 'Accepted' : 'Accept',
+                              style: TextStyle(
+                                color: isAccepted ? Colors.white : Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        height: 40,
+                        width: 115,
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: MaterialButton(
+                          onPressed: () {
+                            // Handle "View on Map" button press
+                          },
+                          child: Text("View on Map"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
             SizedBox(
