0fdbb65e4f5759d3378ce7c8bcf0400a007a94e0
Islam Ahmed <62517820+Hakugami@users.noreply.github.com>
2023-07-09 01:43:36 +0300

    fixes

--- lib/models/Request.dart
+++ lib/models/Request.dart
@@ -7,16 +7,16 @@ class Request {
   UserModel blindData;
   UserLocation blindLocation;
   String state;
-
   String? volunteerId;
   Timestamp date;
 
-  Request(
-      {required this.blindData,
-      required this.blindLocation,
-      this.state = "waiting",
-      this.volunteerId,
-      required this.date});
+  Request({
+    required this.blindData,
+    required this.blindLocation,
+    this.state = "waiting",
+    this.volunteerId,
+    required this.date,
+  });
 
   Map<String, dynamic> toMap() {
     return {
@@ -30,14 +30,36 @@ class Request {
 
   static Request fromJson(Map<String, dynamic> json) {
     return Request(
-        blindData: UserModel.fromJson(json['blindData']),
-        blindLocation: UserLocation.fromJson(json['blindLocation']),
-        state: json['state'],
-        volunteerId: json['volunteerId'],
-        date: json['date']);
+      blindData: UserModel.fromJson(json['blindData']),
+      blindLocation: UserLocation.fromJson(json['blindLocation']),
+      state: json['state'],
+      volunteerId: json['volunteerId'],
+      date: json['date'],
+    );
   }
 
   DateTime _fromTimeStamp() {
     return DateTime.fromMicrosecondsSinceEpoch(date.microsecondsSinceEpoch);
+  }
+}
+
+class UserLocation {
+  double latitude;
+  double longitude;
+
+  UserLocation({required this.latitude, required this.longitude});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'latitude': latitude,
+      'longitude': longitude,
+    };
+  }
+
+  static UserLocation fromJson(Map<String, dynamic> json) {
+    return UserLocation(
+      latitude: double.parse(json['latitude'].toString()),
+      longitude: double.parse(json['longitude'].toString()),
+    );
   }
 }
